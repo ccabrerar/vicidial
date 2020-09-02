8b715fec3842fa478cabee2f02c8a752c059116e
Christian Cabrera <ccabrerar@users.noreply.github.com>
2020-09-02 13:44:13 -0500

    SVN revision 3289

--- install.pl
+++ install.pl
@@ -154,7 +154,7 @@ $secX = time();
 $DB=1;  # Debug flag, set to 0 for no debug messages, lots of output
 $US='_';
 $MT[0]='';
-$svn_revision_fixed = 3279;
+$svn_revision_fixed = 3289;
 
 ### begin parsing run-time options ###
 if (length($ARGV[0])>1)
