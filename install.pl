fef2b41997c22fc731855cbad0715f3fb0f53065
Christian Cabrera <ccabrerar@users.noreply.github.com>
2023-02-14 20:33:09 -0600

    SVN revision 3682

--- install.pl
+++ install.pl
@@ -163,7 +163,7 @@ $secX = time();
 $DB=1;  # Debug flag, set to 0 for no debug messages, lots of output
 $US='_';
 $MT[0]='';
-$svn_revision_fixed = 3539;
+$svn_revision_fixed = 3682;
 
 ### begin parsing run-time options ###
 if (length($ARGV[0])>1)
